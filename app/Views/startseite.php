4611feb2f3592d02f17d524fd6045842a92ff17c
jangreiner <jangreiner38@gmail.com>
2025-11-18 14:13:46 +0100

    Update der Startseite am 18.11.25 JG

---
+++ app/Views/startseite.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Card Beispiel</title>
+    <link rel="stylesheet" type="text/css" href="../../public/css/style.css">
+
+    <!-- Bootstrap 5 CDN -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+    >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<!-----------------------------Navigationsleiste Anfang----------------------------->
+<nav class="navbar navbar-bg px-3">
+    <a class="navbar-brand d-flex align-items-center" href="#">
+        <img src="/public/Images/07_-_WE-Logo.svg" alt="Logo" class="navbar_logo">
+    </a>
+</nav>
+<!-----------------------------Navigationsleiste Ende----------------------------->
+
+<div class="container mt-5" style="display: flex; align-items: center; justify-content: center">
+    <div class="card" style="width: 2200px;">
+
+        <div class="card-body">
+            <h5 class="card-title">Tasks</h5>
+
+        </div>
+    </div>
+</div>
+
+<!-----------------------------Footer Anfang----------------------------->
+<div class ="footer_class">
+    <div class = "footer-content">
+        <div  class="footer-element-left">©Web-Entwicklung Team 08 </div>
+            <div class ="right-group">
+                <div class ="footer-element-right">Impressum </div>
+                <div class ="footer-element-right">Datenschutz </div>
+                <div class ="footer-element-right">Kontakt</div>
+            </div>
+    </div>
+</div>
+</footer>
+<!-----------------------------Footer Ende----------------------------->
+
+<!-- Bootstrap JS (optional) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
+
+
