db853020854fa16b0e6321618323d0434768224c
Phillip Grommes <s4phgrom@uni-trier.de>
2025-11-22 16:30:01 +0100

    Commit 4 PG

--- app/Views/add.php
+++ app/Views/add.php
@@ -17,7 +17,7 @@
 
 <nav class="navbar navbar-bg px-3">
     <div class="d-flex align-items-center h-100">
-        <a href="https://team08.wi1cm.uni-trier.de/public/">
+        <a href="<?=base_url()?>">
             <img class="navbar_logo" src="<?=base_url('Images/07_-_WE-Logo.svg')?>" alt="Logo">
         </a>
         <a class="navbar-items text-decoration-none" href="<?= base_url() ?>">Tasks</a>
