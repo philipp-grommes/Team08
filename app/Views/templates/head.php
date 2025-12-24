aa8c9afdaa8db4bf63932cd58dfb17a145008082
Jan <jangreiner38@gmail.com>
2025-12-24 17:45:19 +0100

    Update responsivität der navBar JG

--- app/Views/templates/head.php
+++ app/Views/templates/head.php
@@ -6,7 +6,7 @@
     <title>Web-Entwicklung Team08</title>
     <link rel="stylesheet" type="text/css" href="<?= base_url('css/style.css')?>">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,11 +1,18 @@
-<nav class="navbar navbar-bg px-3">
-    <div class="d-flex align-items-center h-100">
-        <a href="<?=base_url()?>">
+<nav class="navbar navbar-bg navbar-expand-lg px-3 navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?=base_url()?>">
             <img class="navbar_logo" src="<?=base_url('Images/07_-_WE-Logo.svg')?>" alt="Logo">
         </a>
-        <a class="navbar-items text-decoration-none" href="<?= base_url('tasks') ?>">Tasks</a>
-        <a class="navbar-items text-decoration-none" href="<?= base_url() ?>">Boards</a>
-        <a class="navbar-items text-decoration-none" href="<?= base_url('tables') ?>">Spalten</a>
-        <a class="navbar-items text-decoration-none" href="<?= base_url('personen') ?>">Personen</a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav">
+        <a class=" navbar-link navbar-items text-decoration-none" href="<?= base_url('tasks') ?>">Tasks</a>
+        <a class=" navbar-link navbar-items text-decoration-none" href="<?= base_url() ?>">Boards</a>
+        <a class=" navbar-link navbar-items text-decoration-none" href="<?= base_url('tables') ?>">Spalten</a>
+        <a class=" navbar-link navbar-items text-decoration-none" href="<?= base_url('personen') ?>">Personen</a>
+            </div>
     </div>
 </nav>
